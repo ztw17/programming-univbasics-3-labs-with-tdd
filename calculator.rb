afe859cada91db3efc6e3ed40cd8e8b0b95d010c
Z W <weberzt@gmail.com>
2019-12-05 21:46:28 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,9 @@ first_number = 1.2
 second_number = 2
 
 sum = first_number+second_number
+
+difference = first_number-second_number
+
+product = first_number*second_number
+
+quotient = first_number/second_number
